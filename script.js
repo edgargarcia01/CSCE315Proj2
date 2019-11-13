e923bc3272e08e307e34463a7a656a4e278410b9
edgargarcia <54596619+edgargarciatamu@users.noreply.github.com>
2019-11-12 20:58:49 -0600

    Added IP API
    IP API now functional, automatically gets user city.

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 var inputtext;
 var out;
+var city = 'test';
+var ip = 'check';
+var access_key = '145a6e1f8118cacc7daf96e2b3f5ab42';
+
+
 
 $(document).ready(new function() {                  // similar to int main()
-    //inputtext = document.getElementById("inputtext");
+    getLocation();
     out = document.getElementById("out");
-
-    //out.innerHTML = "testing";
-    
 });
 
-$(document).on('keypress', function(e) {
+$(document).on('keypress', function(e) {            // checks when enter key is pressed in text box
     if(e.which == 13) {
-        checkZip();
+        inputtext = document.getElementById("inputtext").value;
+        
+        outputInputTest();
     }
 });
 
-function checkZip() {
+function outputInputTest() {                        // outputs to site
     inputtext = document.getElementById("inputtext").value;
     out.innerHTML = inputtext;
+}
+
+function getLocation(){
+    $.ajax({
+        url: 'http://api.ipstack.com/' + ip + '?access_key=' + access_key,   
+        dataType: 'jsonp',
+        success: function(json) {
+
+        // output the "capital" object inside "location"
+        //console.log(json);
+                    
+        city = json.city;
+                    
+        $(".city").append(city);
+
+        out.innerHTML = city;
+        
+        }
+    });
 }
