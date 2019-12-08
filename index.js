753bac2a26d6da35e8ad9ee4025873ebac57e259
Emmanuel <emmy0021@tamu.edu>
2019-12-08 12:05:18 -0600

    Added working button link to visit website

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const searchForm = document.querySelector('form');
 const rTemp = document.querySelector('template');
 const resultArea = document.querySelector('#restaurant-results');
-
+const resultFooter = document.getElementById("id01");
 
 
 searchForm.addEventListener('submit', async e => {
@@ -42,17 +42,18 @@ searchForm.addEventListener('submit', async e => {
 function populateData(results) {
   
   results.forEach(result => {
-    //console.log(result);
+    console.log(result);
     const newResult = rTemp.content.cloneNode(true);
     newResult.querySelector('.result-title').innerText = result.restaurant.name;
     newResult.querySelector('.result-neighborhood').innerText = result.restaurant.location.locality;
     newResult.querySelector('.result-address').innerText = result.restaurant.location.address;
     newResult.querySelector('.result-price').innerText = "Cost: " + '$'.repeat(result.restaurant.price_range);
     newResult.querySelector('.result-thumbnail').src = result.restaurant.thumb;
-    //newResult.querySelector('.result-website').onclick = "window.open('" + result.restaurant.url + "','_blank')";
+    newResult.getElementById('result-button').setAttribute( "onClick", "window.open('" + result.restaurant.url + "','_blank')");
     newResult.querySelector('.result-number').innerText = "Phone Number: " + result.restaurant.phone_numbers;
     newResult.querySelector('.result-rating').innerText = result.restaurant.user_rating.aggregate_rating;
     newResult.querySelector('.result-timings').innerText = "Hours: " + result.restaurant.timings;
+   //resultFooter.innerHTML = '<button class="result-footer-button">Visit Website</button>';
     resultArea.appendChild(newResult);
   });
 }
